4267aee5747b77fa175ab51056d1bf6e7ac18a19
smyswzy <buddyinthehood@gmail.com>
2017-05-30 21:03:57 -0700

    Update scrpr.py

--- scrpr.py
+++ scrpr.py
@@ -1,40 +1,90 @@
+import urlparse,urllib,sys,re
 from bs4 import BeautifulSoup
-import urllib,sys,requests,re
+#crawling setup      #
 
-import scrapy 
-
-page3 = urllib.urlopen(sys.argv[1]).read()
-
-print("raw \n\n")
-print(page3)
+if (len(sys.argv) < 2):
+	print('specify url')
+	quit()
 
 
-table =re.findall(r'<table.*?>(.*?)</table.*?>',page3,(re.IGNORECASE|re.DOTALL)) #locate table for data from stdin 
+site ='https://www.1001tracklists.com'
 
-res = []
-line = []
+html = urllib.urlopen(sys.argv[1]).read() 
+#print(html)
+print("~~~~~~~~~~")
 
-res1 = [] 
 
+ll = [] 
+ll.extend((re.findall(r'<tr(.*?)>',html,re.IGNORECASE|re.DOTALL)) )
+#print(ll)
+#for article in page3,  if data-minute in <article  > tag and data-minute!=0 ,grabe minute and grab comment in <p> tag
 print('\n')
-tablecount = 1 
-for t in table:
-		row = re.findall(r'<tr.*?>(.*?)</tr.*?>',t) #locate row per table
 
-		res1.extend((re.findall(r'itemprop="name" content="(.*?)"',t,re.IGNORECASE|re.DOTALL) ) )
+gglinks = [] 
+listCount =0
+for l in ll:
+		 
+		#res1.extend((re.findall(r'<p>(.*?)</p>',t,re.IGNORECASE | re.DOTALL) ) )
+		gglinks.extend((re.findall(r'class=" action" onclick="window.open\(\'(.*?)\',',l,re.IGNORECASE|re.DOTALL)) )
 		
-		#links = re.findall() #locate links		
+		#del gg[0]
+		#del gg[1] 
 
-		for r in row: 
-
-
-			res.append((re.findall(r'<meta(.*?)>',r,re.IGNORECASE|re.DOTALL)))
-			
-			line.extend((re.findall(r'itemprop="name" content="(.*?)"',t,re.IGNORECASE|re.DOTALL) ) )
-
+print("ggLINKS ~~~.  __    .~~~~~")		
+print(gglinks)
+linkCount = 0 
+print('\nlinkCount set @ : !')
 
 
-#print(res)
-print('')
-#print(line)
-print(res1)
+rocks = []
+
+output = [] 
+
+for link in gglinks:
+	rocks = site + link
+	print(rocks)
+	
+
+	pebble = urllib.urlopen(rocks).read()
+
+	table =re.findall(r'<table.*?>(.*?)</table.*?>',pebble,(re.IGNORECASE|re.DOTALL)) #locate table for data from stdin 
+
+	for tt in table: 
+
+		output.extend((re.findall(r'itemprop="name" content="(.*?)"',tt,re.IGNORECASE|re.DOTALL) ) )
+
+
+	
+	linkCount = linkCount + 1
+
+
+print(output)
+
+
+"""
+<tr class=" action" onclick="window.open('/tracklist/1yjslymt/noisia-noisia-radio-s03e17-2017-04-28.html', '_self');" id="tlRow_134651">
+urls = [url] #stack of urls to scrape
+visited = [url] #record of all urls
+
+while len(urls) > 0:
+	try:
+		htmltext = urllib.urlopen(urls[0]).read()
+	except: 
+		print urls[0]
+	
+	soup = BeautifulSoup(htmltext,"lxml")
+	urls.pop(0) # grab first element 
+	print(len(urls))
+	for tag in soup.findAll('a' , href = True):
+		#print(tag['href'])
+		
+		tag['href'] = urlparse.urljoin(url , tag['href'] )
+		#grabbing urls from urls found 
+		
+		#
+		if url in tag['href'] and tag['href'] not in visited: 
+			urls.append(tag['href']) #
+			visited.append(tag['href']) # will have complete record of sites
+print(visited)
+"""		
+		 	
